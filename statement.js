9df86a69314a3ce44b77dff0e99b298dec211964
minho <dnjsalsgh123@gmail.com>
2025-12-21 17:59:28 +0900

    [refactoring] Simplify statement function

--- statement.js
+++ statement.js
@@ -10,24 +10,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
 
     //포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -47,6 +30,29 @@ function statement(invoice, plays) {
 
   return result;
 }
+
+// 한번 공연에 대한 요금 계산
+function amountFor(perf, play) {
+  let thisAmount = 0;
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+  return thisAmount;
+}
+
 
 const result = statement(invoices[0], plays)
 console.log('result = ' , result);
