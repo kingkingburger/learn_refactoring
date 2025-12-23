c1fdb897c80a82373a482ac5439f4104616ebde1
minho <dnjsalsgh123@gmail.com>
2025-12-23 18:16:04 +0900

    [refactoring] inline setting

--- statement.js
+++ statement.js
@@ -9,8 +9,6 @@ function statement(invoice, plays) {
       {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf);
-
     //포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -19,9 +17,8 @@ function statement(invoice, plays) {
     }
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(
-        thisAmount / 100)} (${perf.audience}석)\n`;
-    totalAmount += thisAmount;
+    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+    totalAmount += amountFor(perf);
   }
 
   result += `총액: ${format(totalAmount / 100)}\n`;
