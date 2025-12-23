6a037ffd03e87f85c96923cdd0177f2a83b9259b
minho <dnjsalsgh123@gmail.com>
2025-12-23 18:21:20 +0900

    [refactoring] modify variation name

--- statement.js
+++ statement.js
@@ -9,7 +9,6 @@ function statement(invoice, plays) {
       {style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
   for (let perf of invoice.performances) {
-    // 포인트를 적립한다.
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
@@ -43,20 +42,20 @@ function statement(invoice, plays) {
     }
     return result;
   }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
 }
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
   // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ("comedy" === playFor(perf).type) {
-    volumeCredits += Math.floor(perf.audience / 5);
+  if ("comedy" === playFor(aPerformance).type) {
+    result += Math.floor(aPerformance.audience / 5);
   }
-  return volumeCredits;
+  return result;
 }
 
 
