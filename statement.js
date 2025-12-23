0928b5276cc50a2963bc7ef9a03e33613581d4f3
minho <dnjsalsgh123@gmail.com>
2025-12-23 18:40:41 +0900

    [refactoring] inline variable

--- statement.js
+++ statement.js
@@ -10,9 +10,7 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
-  let totalAmount = appleSauce(invoice,);
-
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(appleSauce(invoice))}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
 
   return result;
