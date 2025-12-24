ad7cae42f4035dd951696bdd5b85953f4d497521
minho <dnjsalsgh123@gmail.com>
2025-12-25 07:22:04 +0900

    [refactoring] detech make html function

--- statement.js
+++ statement.js
@@ -3,6 +3,10 @@ import {invoices} from "./invoices.js";
 
 
 function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays){
   let result = `청구 내역(고객명: ${invoice.customer})\n`;
 
   // 청구 내역을 출력한다.
