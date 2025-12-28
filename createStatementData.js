ef57f2579bfe430858dffa2de074ac16e771a2a9
minho <dnjsalsgh123@gmail.com>
2025-12-28 09:08:57 +0900

    [refactoring] create calculator amount

--- createStatementData.js
+++ createStatementData.js
@@ -1,8 +1,30 @@
 // 공연료 계산기 클래스
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
-    this.aPerformance = aPerformance;
-    this.aPlay = aPlay;
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  // 한번 공연에 대한 요금 계산
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`unknown type: ${this.performance.play.type}`);
+    }
+    return result;
   }
 }
 
@@ -18,7 +40,7 @@ export default function createStatementData(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     let result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -28,26 +50,8 @@ export default function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  // 한번 공연에 대한 요금 계산
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
