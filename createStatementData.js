3c0f4c8fdb5e486406a4bb1fe91887a190e77a40
minho <dnjsalsgh123@gmail.com>
2025-12-29 09:27:16 +0900

    [refactoring] make subclass

--- createStatementData.js
+++ createStatementData.js
@@ -1,6 +1,16 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+
+  }
 }
+
+class TragedyCalculator  extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
 
 // 공연료 계산기 클래스
 class PerformanceCalculator {
