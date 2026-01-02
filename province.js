662fcfa5a8076506c1b850c75e265c1056f38d28
minho <dnjsalsgh123@gmail.com>
2026-01-02 10:43:47 +0900

    [update]: setting Producer access method

--- province.js
+++ province.js
@@ -34,5 +34,25 @@ class Province {
   set demand(arg) {this._demand = parseInt(arg);}
   get price() {return this._price}
   set price(arg) {this._price = parseInt(arg);}
+}
 
+class Producer {
+  constructor(aProvince, data) {
+    this._province = aProvince;
+    this._cost = data.cost;
+    this._name = data.name;
+    this._production = data.production || 0;
+  }
+
+  get name() {return this._name}
+  get cost() {return this._cost}
+  set cost(cost) {this._cost = cost;}
+
+  get production() {return this._production}
+  set production(amountStr) {
+    const amount = parseInt(amountStr);
+    const newProduction = Number.isNaN(amount) ? 0 : amount;
+    this._province.totalProduction += newProduction - this._production;
+    this._produnction = newProduction
+  }
 }
