94579725f10247d8f290e52ced61386fd167bf0b
minho <dnjsalsgh123@gmail.com>
2026-01-02 10:51:59 +0900

    [update]: setting calculating demandValue

--- province.js
+++ province.js
@@ -29,11 +29,35 @@ class Province {
   get name() {return this._name}
   get producers() {return this._producers.slice();}
   get totalProduction() {return this._totalProduction}
-  set totalProduction(arg) {this._totalProduction =arg;}
+  set totalProduction(arg) {this._totalProduction = arg;}
   get demand() {return this._demand}
   set demand(arg) {this._demand = parseInt(arg);}
   get price() {return this._price}
   set price(arg) {this._price = parseInt(arg);}
+
+  // 생산 부족분 계산
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+
+  get profit(){
+      return this.demandValue - this.demandCost;
+  }
+
+  get demandValue() {return this.satisfiedDemand * this.price }
+  get satisfiedDemand () {return Math.min(this._demand, this._totalProduction); }
+  get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+    .sort((a, b) => a.cost - b.cost)
+    .forEach(p => {
+      const constribution = Math.min(remainingDemand, p.production);
+      remainingDemand -= constribution;
+      result += constribution * p.cost;
+    });
+    return result;
+  }
 }
 
 class Producer {
