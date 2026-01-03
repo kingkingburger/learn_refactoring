ade7adc612266fae38f53fa9f571d2e5ec71cec0
minho <dnjsalsgh123@gmail.com>
2026-01-04 08:52:13 +0900

    [test]: using expect

--- province.test.js
+++ province.test.js
@@ -1,9 +1,10 @@
-import {describe, it, assert} from "vitest";
+import {describe, it, assert, expect} from "vitest";
 import {Province, sampleProvinceData} from "./province.js";
 
 describe('province', function() {
   it('shortfall', function () {
     const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
+    expect(asia.shortfall).equal(5);
   });
 })
