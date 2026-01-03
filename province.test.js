719f2fbb5f399393a03f2e4857c8702de9a5eee0
minho <dnjsalsgh123@gmail.com>
2026-01-04 08:56:42 +0900

    [test]: promote to a shared variable

--- province.test.js
+++ province.test.js
@@ -2,14 +2,13 @@ import {describe, it, assert, expect} from "vitest";
 import {Province, sampleProvinceData} from "./province.js";
 
 describe('province', function() {
+  const asia = new Province(sampleProvinceData()); // 중복제거
   it('shortfall', function () {
-    const asia = new Province(sampleProvinceData());
     assert.equal(asia.shortfall, 5);
     expect(asia.shortfall).equal(5);
   });
 
   it('profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   })
 })
