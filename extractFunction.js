2e4e20c93350696fb09dad725855661a055e3a6d
minho <dnjsalsgh123@gmail.com>
2026-01-06 21:56:14 +0900

    [update]: initialize extract function

--- extractFunction.js
+++ extractFunction.js
@@ -1,21 +1,21 @@
 function printOwing(invoice) {
-  printBanner();
-  let outstanding = cacluateOutstanding();
+  let outstanding = 0;
 
-  // 세부 사항 출력
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`)
+  console.log("*****************");
+  console.log("**** 고객 채무 ****");
+  console.log("*****************");
 
-}
-
-function printOwing2(invoice) {
-  printBanner();
-  let outstanding = cacluateOutstanding();
-  printDetails(outstanding);
-
-  function printDetails(outstanding){
-    // 세부 사항 출력
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`)
+  // 미해결 채무(outstanding)를 게산한다.
+  for(const o of invoice.orders){
+    outstanding += o.amount;
   }
+
+  // 마감일(dueDate)를 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+  // 세부 사항을 출력한다.
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
