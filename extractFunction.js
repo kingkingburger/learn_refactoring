0f94d8a58123834b2d426045805abc23cfd7d6ac
minho <dnjsalsgh123@gmail.com>
2026-01-07 19:08:08 +0900

    [refactoring]: extract simple function

--- extractFunction.js
+++ extractFunction.js
@@ -1,9 +1,8 @@
 function printOwing(invoice) {
   let outstanding = 0;
 
-  console.log("*****************");
-  console.log("**** 고객 채무 ****");
-  console.log("*****************");
+  // 서문 출력
+  printPreface();
 
   // 미해결 채무(outstanding)를 게산한다.
   for(const o of invoice.orders){
@@ -15,7 +14,17 @@ function printOwing(invoice) {
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
   // 세부 사항을 출력한다.
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
+}
+
+function printPreface() {
+  console.log("*****************");
+  console.log("**** 고객 채무 ****");
+  console.log("*****************");
 }
