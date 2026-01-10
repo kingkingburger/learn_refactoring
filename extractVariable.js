083a320a8ba3a9e46735fd6688af72cf4959b0ce
minho <dnjsalsgh123@gmail.com>
2026-01-11 08:17:20 +0900

    update: initialize extract variable in class

--- extractVariable.js
+++ extractVariable.js
@@ -11,3 +11,18 @@ const after = () => {
   const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping
 }
+
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {return this._data.quantity};
+  get itemPrice() {return this._data.itemPrice};
+
+  get price() {
+    return order.quantity * order.itmePrice -
+        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        Math.min(order.quantity * order.itemPrice * 0.1, 100);
+  }
+}
