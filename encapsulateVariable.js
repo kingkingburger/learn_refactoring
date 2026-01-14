66c2178eaa41210d5e3ade0ac374b27eca07d73d
minho <dnjsalsgh123@gmail.com>
2026-01-14 19:13:37 +0900

    update: variable encapsulate example

--- encapsulateVariable.js
+++ encapsulateVariable.js
@@ -2,3 +2,10 @@ let defaultOwnerData = {firstName: "마틴", lastName: "파울러"};
 
 export function defaultOwner() {return defaultOwnerData;}
 export function setDefaultOwner(arg) {defaultOwnerData = arg;}
+
+const owner1 = defaultOwner()
+assert.equal("파울러", owner1.lastName, "처음 값 확인");
+const owner2 = defaultOwner()
+owner2.lastName = "파슨스";
+assert.equal("파슨스", owner1.lastName, "owner2를 변경한 후"); // 다름.
+
