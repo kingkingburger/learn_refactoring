0a0aeb00ef939ceb5604d93ac465ce3fe51ed972
minho <dnjsalsgh123@gmail.com>
2026-01-15 22:00:58 +0900

    update: rename variable

--- renameVariable.js
+++ renameVariable.js
@@ -1,12 +1,12 @@
 // 변수를 읽는 곳, 값을 수정하는 곳에서 사용하는 변수는 캡슐화 하자
-let tpHd = "untitled";
+let _title = "untitled";
 
 // const result = `<h1>{tpHd}</h1>`;
 //
 // tphd = obj['ariticleTitle'];
 
-export function title() {return tpHd}
-export function setTitle(arg) {tpHd = arg;}
+export function title() {return _title}
+export function setTitle(arg) {_title = arg;}
 
 
 const result = `<h1>{title()}</h1>`;
