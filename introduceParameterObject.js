3790b69bc271602a7b990322f6e57f261a525b27
minho <dnjsalsgh123@gmail.com>
2026-01-16 20:12:54 +0900

    update: initialize parameter object

--- introduceParameterObject.js
+++ introduceParameterObject.js
@@ -1,9 +1,23 @@
 // 매개변수 객체 만들기
 
-function amountInvoiced(startDate, endDate) {}
-function amountReceived(startDate, endDate) {}
-function amountOverdue(startDate, endDate) {}
+const station = {
+  name: "ZB1",
+  readings: [
+    {temp: 47, time: "2016-11-10 09:10"},
+    {temp: 53, time: "2016-11-10 09:20"},
+    {temp: 58, time: "2016-11-10 09:30"},
+    {temp: 53, time: "2016-11-10 09:40"},
+    {temp: 51, time: "2016-11-10 09:50"},
+  ]
+}
 
-function amountInvoiced(aDataRange) {}
-function amountReceived(aDataRange) {}
-function amountOverdue(aDataRange) {}
+// 정상 범위를 벗어난 측정값을 찾는 함수
+function readingOutsideRange(station, min, max) {
+  return station.readings
+        .filter(r => r.temp < min || r.temp > max);
+}
+
+alerts = readingOutsideRange(station,
+    operationPlan.temperatureFloor, // 최저온도
+    operationPlan.temperatureCeiling, // 최고온도
+    );
