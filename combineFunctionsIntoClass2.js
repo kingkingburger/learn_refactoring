580c40005d017b6024b86f6d961493cccd6dcdfd
minho <dnjsalsgh123@gmail.com>
2026-01-17 08:12:39 +0900

    update: setting method in texableCharge

--- combineFunctionsIntoClass2.js
+++ combineFunctionsIntoClass2.js
@@ -11,11 +11,18 @@ class Reading {
   get year() {return this._year;}
   get month() {return this._month;}
 
-  get calculateBaseCharge() {
+  get baseCharge() {
     return baseRate(this.month, this.year) * this.quantity;
+  }
+
+  get taxableCharge() {
+    return Math.max(0, base - taxThreshold(this.year))
   }
 }
 
 const rawReading = acquireReadting();
 const aReading = new Reading(rawReading);
-const basicChargeAmount = aReading.calculateBaseCharge;
+// class의 필드인지 함수 호출인지 구분하기 어렵게 만드는게 좋다.
+const basicChargeAmount = aReading.baseCharge; // 단일 책임 법칙(uniform Access principle)
+const taxableCharge = aReading.taxableCharge;
+
