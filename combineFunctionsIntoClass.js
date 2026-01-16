61adef4c4fdfed264cb4d28e61aebecac0c885e6
minho <dnjsalsgh123@gmail.com>
2026-01-17 08:04:22 +0900

    update: extract function

--- combineFunctionsIntoClass.js
+++ combineFunctionsIntoClass.js
@@ -1,9 +1,14 @@
 // client 1
 
-const aReading = acquireReadting();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const aReading1 = acquireReadting();
+const baseCharge = calculateBaseCharge(aReading1);
 
 // client2
-const aReading = acquireReadting();
-const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const aReading2 = acquireReadting();
+const base = calculateBaseCharge(aReading2);
 const textableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+// 기본 요금 계산 함수
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
