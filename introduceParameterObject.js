3389c5a3dad4a734a9e6523df9b0423b5e774534
minho <dnjsalsgh123@gmail.com>
2026-01-16 20:09:26 +0900

    update: initialize parameter object

---
+++ introduceParameterObject.js
@@ -0,0 +1,9 @@
+// 매개변수 객체 만들기
+
+function amountInvoiced(startDate, endDate) {}
+function amountReceived(startDate, endDate) {}
+function amountOverdue(startDate, endDate) {}
+
+function amountInvoiced(aDataRange) {}
+function amountReceived(aDataRange) {}
+function amountOverdue(aDataRange) {}
