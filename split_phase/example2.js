65edfc062bea4aec36267e62116b301c35e8aac7
minho <dnjsalsgh123@gmail.com>
2026-01-20 20:48:00 +0900

    update: extract 두 번째 단계

--- split_phase/example2.js
+++ split_phase/example2.js
@@ -1,7 +1,13 @@
 function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-  const shippingPerCase = (basePRice > shippingMethod.discountThreshold) ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+  const price = applyShipping(shippingMethod, quantity, basePrice, discount);
+  return price;
+}
+
+function applyShipping(shippingMethod, quantity, basePrice, discount) {
+  const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+      ? shippingMethod.discountedFee : shippingMethod.feePerCase;
   const shippingCost = quantity * shippingPerCase;
   const price = basePrice - discount + shippingCost;
   return price;
