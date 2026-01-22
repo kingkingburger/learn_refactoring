0f96e462a35fe809bac8eb71133dd23a89653ea9
minho <dnjsalsgh123@gmail.com>
2026-01-22 20:59:00 +0900

    update: 중첩된 예시

---
+++ encapsulate_record/example3.js
@@ -0,0 +1,40 @@
+const exampleJson = {
+  "1920" : {
+    name: "마틴 파울러",
+    id: "1920",
+    usages: {
+      "2016": {
+        "1": 50,
+        "2": 55,
+      },
+      "2015": {
+        "1": 70,
+        "2": 63
+      }
+    }
+  },
+  "38673" : {
+    name: "닐 포드",
+    id: "38673",
+    usages: {
+      "2016": {
+        "1": 50,
+        "2": 55,
+      },
+      "2015": {
+        "1": 70,
+        "2": 63
+      }
+    }
+  }
+}
+
+// 쓰기 예
+exampleJson[customerID].usages[year][month] = amount;
+
+// 읽기 예
+function compareUsage (customerID, laterYear, month) {
+  const later = customerData[customerID].usages[laterYear][month];
+  const earlier = customerData[customerID].usages[laterYear-1][month];
+  return {laterAmount: later, change: later - earlier};
+}
