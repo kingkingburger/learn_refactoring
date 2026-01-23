c255a49daa8b23b5ffc4c7c937c5f58b3f61020e
minho <dnjsalsgh123@gmail.com>
2026-01-24 06:35:12 +0900

    update: Priority 클래스 생성

--- replace_primitive_with_object/example2.js
+++ replace_primitive_with_object/example2.js
@@ -6,3 +6,8 @@ class Order{
   get priority() {return this._priority;}
   set priority(priority) {this._priority = priority;}
 }
+
+class Priority {
+  constructor(value) { this._value = value; }
+  toString() { return this._value; }
+}
