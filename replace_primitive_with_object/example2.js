4d7d5460d309a478a03e7937d92a13ad27b03df0
minho <dnjsalsgh123@gmail.com>
2026-01-24 06:44:38 +0900

    update: order에 priority 인스턴스를 둔다.
    - priority 클래스는 다른 곳에서도 유용하게 쓸 수 있다
    - 객체 안의 값이 매개변수와 비교하는 로직으로 만들면 유연한 클래스가 된다

--- replace_primitive_with_object/example2.js
+++ replace_primitive_with_object/example2.js
@@ -3,11 +3,23 @@ class Order{
     this._priority = data.priority;
   }
 
-  get priority() {return this._priority.toString();}
+  get priority() {return this._priority;}
+  get priorityString() {return this._priority.toString();}
   set priority(aString) {this._priority = new Priority(aString);}
 }
 
 class Priority {
-  constructor(value) { this._value = value; }
+  constructor(value) {
+    if(value instanceof Priority) {return value;}
+    if(Priority.legalValue().includes(value))
+      this._value = value;
+    else
+      throw new Error(`${value} is not a Priority.legalValue()`);
+  }
   toString() { return this._value; }
+  get _index() {return Priority.legalValue().findIndex(s => s === this._value); }
+  static legalValue() {return ['low', 'normal', 'high' , 'rush'];}
+  equals(other) { return this._index === other._index; }
+  higherThan(other) { return this._index > other._index; }
+  lowerThan(other) { return this._index < other._index; }k
 }
