dd3f797e84e41e7d31e7e6c05d6ad5f3766f1ef2
minho <dnjsalsgh123@gmail.com>
2026-01-23 20:02:26 +0900

    update: class의 메서드를 활용해서 컬랙션을 수정하게 만들기

--- encapsulate_collection/example1.js
+++ encapsulate_collection/example1.js
@@ -1,3 +1,6 @@
+// 컬랙션 => list, array, dict 같은 가변형 데이터 집합들
+// class로 묶여도 통채로 변경될 가능성 있음
+
 class Person {
   constructor(name) {
     this._name = name;
@@ -7,6 +10,15 @@ class Person {
   get name () { return this._name; }
   get courses () { return this._courses; }
   set courses (value) { this._courses = value; }
+
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourse(aCourse, fnIfAbsent = () => { throw new RangeError()}) {
+    const index = this._courses.indexOf(aCourse);
+    if (index > -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
+  }
 }
 
 class Course {
