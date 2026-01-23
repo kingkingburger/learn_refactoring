f7f14ac9124717471c43d7f888008f450e5afe2f
minho <dnjsalsgh123@gmail.com>
2026-01-23 19:59:35 +0900

    update: initialize 컬랙션 캡슐화 하기

---
+++ encapsulate_collection/example1.js
@@ -0,0 +1,20 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+    this._courses = [];
+  }
+
+  get name () { return this._name; }
+  get courses () { return this._courses; }
+  set courses (value) { this._courses = value; }
+}
+
+class Course {
+  constructor(name, isAdvanced ) {
+    this._name = name;
+    this._isAdvanced = isAdvanced;
+  }
+
+  get name () { return this._name; }
+  get isAdvanced () { return this._isAdvanced; }
+}
