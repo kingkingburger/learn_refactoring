179fc1531ff1fe9b184fea94c8f5b9e987a18eb4
minho <dnjsalsgh123@gmail.com>
2026-01-26 22:28:25 +0900

    update: 새로운 클래스의 메서드로 바꿔주기

--- extract_class/example1.js
+++ extract_class/example1.js
@@ -4,13 +4,17 @@ class Person {
   }
   get name() { return this._name; }
   set name (value) { this._name = value; }
-  get telephoneNumber() { return `${this.officeAreaCode} ${this.officeNumber}`; }
-
-  get officeNumber() {return this._officeNumber;}
-  set officeNumber (value) { this._officeNumber = value; }
+  get telephoneNumber() { return `${this._telephoneNumber.officeAreaCode} ${this._telephoneNumber.officeNumber}`; }
+  get officeAreaCode() {return this._telephoneNumber._officeAreaCode; }
+  set officeAreaCode (value) { this._telephoneNumber._officeAreaCode = value; }
+  get officeNumber() {return this._telephoneNumber._officeNumber;}
+  set officeNumber (value) { this._telephoneNumber._officeNumber = value; }
 }
 
 class TelephoneNumber {
+  get telephoneNumber() { return `${this.officeAreaCode} ${this.officeNumber}`; }
   get officeAreaCode() {return this._officeAreaCode; }
   set officeAreaCode (value) { this._officeAreaCode = value; }
+  get officeNumber() {return this._officeNumber;}
+  set officeNumber (value) { this._officeNumber = value; }
 }
