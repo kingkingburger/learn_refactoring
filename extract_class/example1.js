75132acd8f02f524b03612dcb767fcca86011324
minho <dnjsalsgh123@gmail.com>
2026-01-26 22:25:04 +0900

    update: initialize 예제 class

---
+++ extract_class/example1.js
@@ -0,0 +1,9 @@
+class Person {
+  get name() { return this._name; }
+  set name (value) { this._name = value; }
+  get telephoneNumber() { return `${this.officeAreaCode} ${this.officeNumber}`; }
+  get officeAreaCode() {return this._officeAreaCode; }
+  set officeAreaCode (value) { this._officeAreaCode = value; }
+  get officeNumber() {return this._officeNumber;}
+  set officeNumber (value) { this._officeNumber = value; }
+}
