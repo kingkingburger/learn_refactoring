34dff91caed219ae01ade2e4bb5b4c6d5846b0fd
minho <dnjsalsgh123@gmail.com>
2026-01-26 22:26:39 +0900

    update: 생성자로 새로운 클래스 저장하기

--- extract_class/example1.js
+++ extract_class/example1.js
@@ -1,13 +1,16 @@
 class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
   get name() { return this._name; }
   set name (value) { this._name = value; }
   get telephoneNumber() { return `${this.officeAreaCode} ${this.officeNumber}`; }
-  get officeAreaCode() {return this._officeAreaCode; }
-  set officeAreaCode (value) { this._officeAreaCode = value; }
+
   get officeNumber() {return this._officeNumber;}
   set officeNumber (value) { this._officeNumber = value; }
 }
 
 class TelephoneNumber {
-
+  get officeAreaCode() {return this._officeAreaCode; }
+  set officeAreaCode (value) { this._officeAreaCode = value; }
 }
