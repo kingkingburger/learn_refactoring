054b82de3727d191345c633244b8f721e3b86d7d
minho <dnjsalsgh123@gmail.com>
2026-01-27 20:45:57 +0900

    docs: 소감

--- inline_class/example1.js
+++ inline_class/example1.js
@@ -18,3 +18,7 @@ class Shipping {
   get trackingNumber() {return this._shippingCompany;}
   set trackingNumber (value) { this._shippingCompany = value; }
 }
+
+// 이건 그래도 어떤 클래스를 옮겨야 하는지 명확한 기준이 있다.
+// 더 이상 제 역할을 못해서 그대로 두면 안되는 클래스를 타겟으로 잡는다.
+// 예전에는 유용했을 코드가 존재한다. 코드가 살아있다는 증거이다.
