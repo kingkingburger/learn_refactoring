a834a6097c8d92ae439804ff30f599e2692c338f
minho <dnjsalsgh123@gmail.com>
2026-01-27 20:44:36 +0900

    docs: shipping 클래스에 trackingInformation에서 사용하는 메서드들 옮기기

--- inline_class/example1.js
+++ inline_class/example1.js
@@ -1,19 +1,20 @@
-class TrackingInformation {
-  get shippingCompany() { return this._shippingCompany;}
-  set shippingCompany (value) { this._shippingCompany = value; }
-  get trackingNumber() {return this._trackingNumber;}
-  set trackingNumber (value) { this._trackingNumber = value; }
-  get display() {
-    return `${this.shippingCompany} ${this.trackingNumber}`;
-  }
-}
+// class TrackingInformation {
+//   get shippingCompany() { return this._shippingCompany;}
+//   set shippingCompany (value) { this._shippingCompany = value; }
+//   get trackingNumber() {return this._trackingNumber;}
+//   set trackingNumber (value) { this._trackingNumber = value; }
+//   get display() {
+//     return `${this.shippingCompany} ${this.trackingNumber}`;
+//   }
+// }
 
 class Shipping {
   get trackingInfo() {
-    return this._trackingInfomation.display;
+    return `${this.shippingCompany} ${this.trackingNumber}`;
   }
-  get trackingInformation() {return this._trackingInfomation}
-  set trackingInformation (value) { this._trackingInfomation = value; }
-  get shippingCompany() { return this._trackingInfomation.shippingCompany; }
-  set shippingCompany (value) { this._trackingInfomation.shippingCompany = value; }
+
+  get shippingCompany() { return this._shippingCompany; }
+  set shippingCompany (value) { this._shippingCompany = value; }
+  get trackingNumber() {return this._shippingCompany;}
+  set trackingNumber (value) { this._shippingCompany = value; }
 }
