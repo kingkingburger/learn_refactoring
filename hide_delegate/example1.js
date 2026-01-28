f6cdf9db31d7d3569ed380cf0bc552a237165c56
minho <dnjsalsgh123@gmail.com>
2026-01-28 20:51:06 +0900

    docs: 소감

--- hide_delegate/example1.js
+++ hide_delegate/example1.js
@@ -20,3 +20,7 @@ class Department{
 // manager = aPerson.department.manager; // 이렇게 불러와야 한다.
 
 manager = aPerson.manager; // 이렇게 불러올 수 있다.
+
+// 이렇게 하면 부서객체가 캡슐화가 된 것이다.
+// Person class가 Department class의 존재를 숨겨준다.
+// department가 외부에 자주 쓰이는 상황이 않이면 좋은것 같네요
