b58e09b9fc91ec85defc4ebe884c3e848786c706
minho <dnjsalsgh123@gmail.com>
2026-01-31 10:25:09 +0900

    update: 함수 밖으로 빼기

--- move_function/example1.js
+++ move_function/example1.js
@@ -1,6 +1,6 @@
 function trackSummary(points) {
   const totalTime = calculateTime();
-  const totalDistance = calculateDistance();
+  const totalDistance = totalDistance(points);
   const pace = totalTime / 60 / totalDistance;
   return {
     time: totalTime,
@@ -8,15 +8,17 @@ function trackSummary(points) {
     pace: pace
   };
 
-  function calculateDistance() {
-    let result = 0;
-    for (let i = 1; i < points.length; i += 1) {
-      result += distance(points[i-1] , points[i]);
-    }
-    return result;
-  }
+
 
   function distance(p1, p2) {}
   function radians(degrees) {}
   function calculateTime() {}
+}
+
+function totalDistance(point) {
+  let result = 0;
+  for (let i = 1; i < points.length; i += 1) {
+    result += distance(points[i-1] , points[i]);
+  }
+  return result;
 }
