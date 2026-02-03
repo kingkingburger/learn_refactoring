0b9261ad63c6d26a37ac6b8854c52d04f7152d63
minho <dnjsalsgh123@gmail.com>
2026-02-03 20:14:06 +0900

    update: 공유 객체 이동하기

--- move_field/example2.js
+++ move_field/example2.js
@@ -2,14 +2,17 @@ class Account {
   constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    assert(interestRate === this._type.interestRate);
+  }
+
+  get interestRate() {return this._type.interestRate; }
+}
+
+class AccountType {
+  constructor(nameString, interestRate) {
+    this._name = nameString;
     this._interestRate = interestRate;
   }
 
   get interestRate() {return this._interestRate; }
-}
-
-class AccountType {
-  constructor(nameString) {
-    this._name = nameString;
-  }
 }
