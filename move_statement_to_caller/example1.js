62181b348fdcb7f6dfe0360fcdb582ee40831a69
minho <dnjsalsgh123@gmail.com>
2026-02-05 21:22:16 +0900

    update: initiailize 문장을 호출한 곳으로 옮기기

--- move_statement_to_caller/example1.js
+++ move_statement_to_caller/example1.js
@@ -8,16 +8,14 @@ function listRecentPhotos(outStream, photos){
   photos
   .filter(p => p.date > recentDateCutoff())
   .forEach(p => {
-    outStrea.write("<div>\n");
+    outStream.write("<div>\n");
     emitPhotoData(outStream, p);
     outStream.write("</div>\n");
   })
 }
 
 function emitPhotoData(outStream, p) {
-  return [
-    `<p>제목: ${p.title}</p>`,
-    `<p>위치: ${p.location}</p>`,
-    `<p>날짜: ${p.date.toDateString()}</p>`,
-  ].join("\n");
+  outStream.write(`<p>${p.name}</p>\n`);
+  outStream.write(`<p>날짜: ${p.date.toDateString()}</p>\n`);
+  outStream.write(`<p>위치: ${p.location}`)
 }
