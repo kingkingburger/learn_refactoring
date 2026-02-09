f14b47554a253ab015042714e81742c1468bca14
minho <dnjsalsgh123@gmail.com>
2026-02-09 22:59:25 +0900

    update: initialize 반복문을 파이프 라인으로 바꾸기

---
+++ replace_loop_with_pipeline/example1.js
@@ -0,0 +1,16 @@
+function acquireData(input) {
+  const lines = input.split('\n');
+  let firstLine = true;
+  const result = [];
+  for (const line of lines) {
+    if (firstLine) {
+      firstLine = false;
+      continue;
+    }
+    if(line.trim() === "") continue;
+    if (recore[1].trim() === "India") {
+      result.push({city: record[0].trim(), phone: record[2].trim()});
+    }
+  }
+  return result;
+}
