b6d5aba1cd2962cf1fc0c1c044a21ed0d1ed94f4
minho <dnjsalsgh123@gmail.com>
2026-02-09 23:05:19 +0900

    update: "India" 문자만 있는 배열 만들기 컬렉션 파이프라인으로 변경

--- replace_loop_with_pipeline/example1.js
+++ replace_loop_with_pipeline/example1.js
@@ -4,13 +4,12 @@ function acquireData(input) {
   const loopItems = lines
       .slice(1)
       .filter(line => line.trim() !== "")
-      .map(line => line.trim())
+      .map(record => record.trim())
+      .filter(record => record[1] === "India")
   ;
   for (const line of lines) {
     const record = line;
-    if (record[1].trim() === "India") {
       result.push({city: record[0].trim(), phone: record[2].trim()});
-    }
   }
   return result;
 }
