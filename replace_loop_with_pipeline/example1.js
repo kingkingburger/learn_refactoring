155a2ea3eaee9e435d44edbec21e463a81a9667f
minho <dnjsalsgh123@gmail.com>
2026-02-09 23:01:04 +0900

    update: 첫 줄 건너뛰기 컬렉션 파이프라인화

--- replace_loop_with_pipeline/example1.js
+++ replace_loop_with_pipeline/example1.js
@@ -1,13 +1,11 @@
 function acquireData(input) {
   const lines = input.split('\n');
-  let firstLine = true;
   const result = [];
-  const loopItems = lines;
+  const loopItems = lines
+      .slice(1)
+  ;
   for (const line of lines) {
-    if (firstLine) {
-      firstLine = false;
-      continue;
-    }
+
     if(line.trim() === "") continue;
     if (recore[1].trim() === "India") {
       result.push({city: record[0].trim(), phone: record[2].trim()});
