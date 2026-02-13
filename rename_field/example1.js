5d4a1701a44b254aeafcccc2bc79c127253a0b69
minho <dnjsalsgh123@gmail.com>
2026-02-14 08:55:25 +0900

    update: initialize 필드이름 바꾸기

---
+++ rename_field/example1.js
@@ -0,0 +1,12 @@
+class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(value) {this._name = value;}
+  get country() {return this._country;}
+  set country(value) {this._country = value;}
+}
