e2cded6a65a54a2f248154173d69db75bc9fd3c9
minho <dnjsalsgh123@gmail.com>
2026-02-14 08:58:03 +0900

    docs: 소감

--- rename_field/example1.js
+++ rename_field/example1.js
@@ -10,3 +10,7 @@ class Organization {
   get country() {return this._country;}
   set country(value) {this._country = value;}
 }
+
+// 호환성을 유지하면서 테스트를 처리한다.
+// 호출하는 곳에서 변수를 바꾸면 호환성을 제거한다.
+// 요즘은 ai 시키면 될듯
