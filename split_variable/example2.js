e0d395e370b91f86de147bcdb8c0ca32b8e2cb8e
minho <dnjsalsgh123@gmail.com>
2026-02-13 18:40:52 +0900

    docs: 소감

--- split_variable/example2.js
+++ split_variable/example2.js
@@ -4,3 +4,7 @@ function discount (inputValue, quantity) {
   if (quantity > 100) result = result - 1;
   return result;
 }
+
+// 변수 쪼개기로 입력매개변수를 처리할 수 있다.
+// 호출자에 영향을 주지 않기 때문에 함수에 데이터를 전달하는 용도, 결과를 호출자에 반환하는 용도를 나눌 수 있다.
+// 변수의 흐름을 처리하기 쉬운상태가됨
