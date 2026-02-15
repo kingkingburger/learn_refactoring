230deba9d8aebe4bb409c43886a683eafd76d026
minho <dnjsalsgh123@gmail.com>
2026-02-15 18:21:44 +0900

    update: djtjtus cnrkgotj xptmxm goqhrl

--- replace_derived_variable_with_query/exampl1.js
+++ replace_derived_variable_with_query/exampl1.js
@@ -1,5 +1,14 @@
 class ProductionPlan {
-  get production() {return this._production;}
+  get production() {
+    assert(this._production === this.calcaultedProduction);
+    return this._production;
+  }
+
+  get calcaultedProduction() {
+    return this._adjustments
+      .reduce((sum,a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
     this._production += adjustment.amount;
