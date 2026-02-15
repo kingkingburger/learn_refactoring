28511f61e964e3d3a4c9b37a573748a498f942cd
minho <dnjsalsgh123@gmail.com>
2026-02-16 07:36:10 +0900

    update: 변수 쪼개기 적용

--- replace_derived_variable_with_query/example2.js
+++ replace_derived_variable_with_query/example2.js
@@ -1,14 +1,15 @@
 class ProductionPlan {
   constructor(product) {
-    this._production = product;
+    this._initialProduction = product;
+    this._productionAcceumulator = 0;
     this._adjustments = [];
   }
   get production() {
-    return this._production;
+    return this._initialProduction + this._productionAcceumulator;
   }
 
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
+    this._productionAcceumulator += adjustment.amount;
   }
 }
