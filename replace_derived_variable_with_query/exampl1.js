b2601cf57984c0a53b3d51cc1ec4a25dd8a1ac41
minho <dnjsalsgh123@gmail.com>
2026-02-15 18:22:54 +0900

    update: 인라인, 죽은 코드 제거하기

--- replace_derived_variable_with_query/exampl1.js
+++ replace_derived_variable_with_query/exampl1.js
@@ -1,15 +1,10 @@
 class ProductionPlan {
   get production() {
-    return this.calculatedProduction;
-  }
-
-  get calculatedProduction() {
     return this._adjustments
-      .reduce((sum,a) => sum + a.amount, 0);
+    .reduce((sum, a) => sum + a.amount, 0);
   }
 
   applyAdjustment(adjustment) {
     this._adjustments.push(adjustment);
-    this._production += adjustment.amount;
   }
 }
