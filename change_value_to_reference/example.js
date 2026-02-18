90aea539bda71a00e6a5f55331576e134ee34681
minho <dnjsalsgh123@gmail.com>
2026-02-18 11:51:36 +0900

    update: initialize 값을 참조로 바꾸기

---
+++ change_value_to_reference/example.js
@@ -0,0 +1,15 @@
+class Order {
+  constructor(data) {
+    this._number = data.number;
+    this._customer = new Customr(data.customer);
+  }
+  get customer() {return this._customer;}
+}
+
+class Customer {
+  constructor(id) {
+    this._id = id;
+  }
+
+  get id() {return this._id;}
+}
