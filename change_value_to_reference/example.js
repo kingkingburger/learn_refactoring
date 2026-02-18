1cf07a4c02dea7d619dcbf16601899bd7138f4c8
minho <dnjsalsgh123@gmail.com>
2026-02-18 11:53:27 +0900

    update: repository 객체로 객체 저장소 만들기

--- change_value_to_reference/example.js
+++ change_value_to_reference/example.js
@@ -1,3 +1,21 @@
+let repositoryData;
+
+function initialize() {
+  _repositoryData = {};
+  _repositoryData.customer = new Map();
+}
+
+function registerCustomer(id) {
+  if(!repositoryData.customer.has(id)) {
+    repositoryData.customer.set(id, new Customer(id));
+  }
+  return findCustomer(id);
+}
+
+function findCustomer(id) {
+  return _repositoryData.customer.get(id);
+}
+
 class Order {
   constructor(data) {
     this._number = data.number;
