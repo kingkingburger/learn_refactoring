3ff80767479e6cfb45149e797dc122ce28d35ba7
minho <dnjsalsgh123@gmail.com>
2026-02-28 12:51:56 +0900

    마지막 result 변수 제거(가변변수 제거)

--- replace_nested_conditional_with_guard_clauses/example1.js
+++ replace_nested_conditional_with_guard_clauses/example1.js
@@ -1,13 +1,9 @@
 function payAmount(employee) {
-    let result;
-    if(employee.isSeparated) return {amount: 0, reasoneCode: "SEP"};
+    if (employee.isSeparated) return {amount: 0, reasoneCode: "SEP"};
     if (employee.isRetired) return  {amount: 0, reasonCode: "RET"};
-    else {
-        lorem.ipsum(dolor.sitAmet);
-        consectetur(adipiscing).elit();
-        setDefaultOwner.doeiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-        ut.enim.ad(minim.veniam);
-        result = someFinalComputation();
-    }
-    return result;
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    setDefaultOwner.doeiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(minim.veniam);
+    return someFinalComputation();
 }
