6d8a3d5904a367282608f47479f562ff28bf7eb9
minho <dnjsalsgh123@gmail.com>
2026-02-28 12:48:37 +0900

    initialize example

---
+++ replace_nested_conditional_with_guard_clauses/example1.js
@@ -0,0 +1,19 @@
+function payAmount(employee) {
+    let result;
+    if(employee.isSeparated) {
+        result = {amount: 0, reasoneCode: "SEP"};
+    }
+    else {
+        if (employee.isRetired) {
+            result = {amount: 0, reasonCode: "RET"}
+        }
+        else {
+            lorem.ipsum(dolor.sitAmet);
+            consectetur(adipiscing).elit();
+            setDefaultOwner.doeiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+            ut.enim.ad(minim.veniam);
+            result = someFinalComputation();
+        }
+    }
+    return result;
+}
