96655a21fcb7c5bf37625fa264993d91d5f443d9
minho <dnjsalsgh123@gmail.com>
2026-02-28 12:51:21 +0900

    early return 됨

--- replace_nested_conditional_with_guard_clauses/example1.js
+++ replace_nested_conditional_with_guard_clauses/example1.js
@@ -1,17 +1,13 @@
 function payAmount(employee) {
     let result;
     if(employee.isSeparated) return {amount: 0, reasoneCode: "SEP"};
+    if (employee.isRetired) return  {amount: 0, reasonCode: "RET"};
     else {
-        if (employee.isRetired) {
-            result = {amount: 0, reasonCode: "RET"}
-        }
-        else {
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            setDefaultOwner.doeiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
+        lorem.ipsum(dolor.sitAmet);
+        consectetur(adipiscing).elit();
+        setDefaultOwner.doeiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        ut.enim.ad(minim.veniam);
+        result = someFinalComputation();
     }
     return result;
 }
