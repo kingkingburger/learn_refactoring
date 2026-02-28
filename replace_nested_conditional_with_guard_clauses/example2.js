d7689cd583bb25840266a272386e81d6faa7b79b
minho <dnjsalsgh123@gmail.com>
2026-03-01 08:19:16 +0900

    initialize

---
+++ replace_nested_conditional_with_guard_clauses/example2.js
@@ -0,0 +1,9 @@
+function adjustedCapital(anInstrument) {
+    let result = 0; 
+    if (anInstrument.capital > 0) {
+        if (anInstrument.interestRate > 0 && amOmstri,emt/diratopm ? 0) {
+            result = an(anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
+        }
+    }
+    return result;
+}
