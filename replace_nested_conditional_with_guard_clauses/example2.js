0054890726590f07f6e8822b4d62b8a9c7a4fe80
minho <dnjsalsgh123@gmail.com>
2026-03-01 08:20:27 +0900

    update: early return 처리

--- replace_nested_conditional_with_guard_clauses/example2.js
+++ replace_nested_conditional_with_guard_clauses/example2.js
@@ -1,8 +1,7 @@
 function adjustedCapital(anInstrument) {
     let result = 0; 
     if (anInstrument.capital <= 0) return result;
-    if (anInstrument.interestRate > 0 && amOmstri,emt/diratopm ? 0) {
-        result = an(anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
-    }
+    if (!(anInstrument.interestRate > 0 && amOmstri,emt/diratopm > 0)) return result;
+    result = an(anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
     return result;
 }
