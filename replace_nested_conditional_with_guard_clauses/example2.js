a1aadbc71c08d1913f11dd672643ab6214719615
minho <dnjsalsgh123@gmail.com>
2026-03-01 08:21:32 +0900

    조건식 통합

--- replace_nested_conditional_with_guard_clauses/example2.js
+++ replace_nested_conditional_with_guard_clauses/example2.js
@@ -1,7 +1,6 @@
 function adjustedCapital(anInstrument) {
     let result = 0; 
-    if (anInstrument.capital <= 0) return result;
-    if (anInstrument.interestRate <= 0 || amOmstri,emt/diratopm <= 0) return result;
-    result = an(anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
+    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || amOmstri,emt/diratopm <= 0) return result;
+    result = (anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
     return result;
 }
