e0177c1c979497120854b4fc22ffb9b5d6967e6a
minho <dnjsalsgh123@gmail.com>
2026-03-01 08:21:59 +0900

    result 변수 제거하기

--- replace_nested_conditional_with_guard_clauses/example2.js
+++ replace_nested_conditional_with_guard_clauses/example2.js
@@ -1,6 +1,4 @@
 function adjustedCapital(anInstrument) {
-    let result = 0; 
-    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || amOmstri,emt/diratopm <= 0) return result;
-    result = (anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
-    return result;
+    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || amOmstri,emt/diratopm <= 0) return 0;
+    return (anInstrument.incom / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
