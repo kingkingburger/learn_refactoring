adb1873937c48cf4af41ed8338163b75ba5db66a
minho <dnjsalsgh123@gmail.com>
2026-03-02 08:28:19 +0900

    update: plumage 오버라이드

--- replace_conditional_witdh_polymorphism/example1.js
+++ replace_conditional_witdh_polymorphism/example1.js
@@ -33,9 +33,17 @@ class EuropeanSwallow extends Bird{
     }
 }
 
-class AfricanSwallow extends Bird{}
+class AfricanSwallow extends Bird{
+    get plumage() {
+        return (this.numberOfCoconuts > 2) ? "지쳣다" : "보통이다"
+    }
+}
 
-class NorwegianBlueSwallow extends Bird{}
+class NorwegianBlueSwallow extends Bird{
+    get plumage() {
+        return (this.voltage > 100) ? "그을렸다" : "예쁘다"
+    }
+}
 
 class Bird {
     constructor(birdObject) {
@@ -43,17 +51,8 @@ class Bird {
     }
     
     get plumage() {
-        switch (this.type) {
-            case '유럽 제비':
-                throw "오류 발생";
-            case '아프리카 제비':
-                return (this.numberOfCoconuts > 2) ? "지쳣다" : "보통이다";
-            case '노르웨이 파랑 앵무': 
-                return (this.voltage > 100) ? "그을렸다" : "예쁘다";
-            default:
-                return "알 수 없다";
-        }
-}
+        return "알 수 없다";
+    }
 
     get airSpeedVelocity() {
         switch(this.type){
